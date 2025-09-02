5c9cf66eea6dbc935554499a1fb9ee51d6174fa6
Grigioo <8grigioo@gmail.com>
2025-09-03 00:04:35 +0300

    Edit removeFromArray.js. Main information is about multiple variable of function.

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+// Example with adition arguments insteed of rest parametrs for more values
+// const removeFromArray = function(array) {
+//     const arrayArguments = [...arguments];
+//     return array.filter(item => !arrayArguments.includes(item));
+// };
+
+const removeFromArray = function(array, ...args) {
+    return array.filter(item => !args.includes(item));
 };
 
 // Do not edit below this line

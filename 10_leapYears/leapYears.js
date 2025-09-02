bbb8a11cadc6c01b539c0b775036cb6db3d8fb24
Grigioo <8grigioo@gmail.com>
2025-09-03 00:43:54 +0300

    Edit leapYears.js. Add solution with main point in boolean operators.

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
+// const leapYears = function(year) {
+//     if (((year % 4 === 0) && (!(year % 100 === 0))) || ((year % 4 === 0) && (year % 400 === 0))) return true;
+//     else return false;
+// };
 
+const leapYears = function(year) {
+    isDivibableByFour = year % 4 === 0;
+    isCentury = year % 100 === 0;
+    isDivibableByFourHundred = year % 400 === 0;
+    
+    if (isDivibableByFour && (!isCentury || isDivibableByFourHundred)) return true;
+    else return false;
 };
 
 // Do not edit below this line

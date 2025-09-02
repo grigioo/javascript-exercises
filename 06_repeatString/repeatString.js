4ed72cf0479847ef84edbb042a86c3caf781ebbc
Grigioo <8grigioo@gmail.com>
2025-09-02 23:37:19 +0300

    Edit repeatString.js. Use for-loop to repeate stroke n times.

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(stroke, count) {
+    let strokeNew = '';
+    if (count < 0) {
+        return 'ERROR';
+    }
+    for (let i = 0; i < count; i++) {
+        strokeNew += stroke;
+    }
+    return strokeNew;
 };
 
 // Do not edit below this line

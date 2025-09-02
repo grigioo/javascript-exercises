64fbfe3b59d5548e464ab856d697cbee72eae7d3
Grigioo <8grigioo@gmail.com>
2025-09-03 00:23:15 +0300

    Edit sumAll.js. Add solution, main target is conditions for type of input.

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(first, last) {
+    if (typeof(first) !== 'number' || typeof(last) !== 'number') return 'ERROR';
+    else if (first < 0) return 'ERROR';
+    else if (Math.round(last) !== last || Math.round(first) !== first) return 'ERROR';
+    else if (first > last) [[first],[last]] = [[last],[first]];
+    let currentNumber = first;
+    let sum = 0;
 
+    while (currentNumber <= last) {
+        sum += currentNumber;
+        currentNumber++;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
